4c79da0cf8366e6cb7dc3107ff4cd1dc9fa2a44a
Alessandro Dal Mas <Alessandrogiovannidalmas@gmail.com>
2026-05-30 17:26:28 +0200

    Initial Commit -Changed file name with my student_ID -Added gitignore

--- s334184.py
+++ s334184.py
@@ -1,8 +1,11 @@
 from Problem import Problem
 
+print("Hello world!")
+
 def solution(p:Problem):
     p=Problem()
     # TODO: implement your solution here
     # In the enf of the code for your solution, make sure that the path has the format [(c1, g1), (c2, g2), …, (cN, gN), (0, 0)]
     path = [(1, 75), (0, 0)]
     return path
+
